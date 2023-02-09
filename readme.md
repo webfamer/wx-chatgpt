4798d7b3246710fa0e0d41f312f8eff12d66ae9b
xsz <webfamer@163.com>
2023-02-09 17:29:37 +0800

    feature(小姐姐):添加小姐姐接口

--- readme.md
+++ readme.md
@@ -6,6 +6,13 @@
 ** 记得把.env下的openai密钥换成你自己的**  
 ** openai接口没返回消息的话，就是被墙了，要挂下小飞机**  
 
+## dev
+添加了聊天开关，以及美女图片接口
+
+其他娱乐功能可按照例子添加
+常用免费api网站：https://apibug.cn
+
+可爱猫插件分享：https://www.123pan.com/s/Tio9-b7zv3 提取码:6TFw
 完整教程：https://juejin.cn/post/7184626139645050935
    
 有问题联系掘金:https://juejin.cn/user/2752832846696477  
--- src/server.js
+++ src/server.js
@@ -2,32 +2,86 @@ let express = require('express');
 let { parseBodyData } = require('./utils.js');
 let { getOpenAiReply } = require('./openai/index.js')
 let app = express();
+//聊天开关
+let chatFlag = false
 app.post('/api/list', async function (req, res) {
     //不能正确解析json 格式的post参数
     let data = await parseBodyData(req)
     console.log(data, 'data')
-    /**微信聊天**/
-        let reg = /wxid=(.*)]/g
-        reg.test(data.msg)
-        let callSomeoneId = RegExp.$1
-        if (data?.msg?.indexOf('@at') > -1 && callSomeoneId == data.robot_wxid) { //要@机器人才聊天
-            const newMsg = data.msg.substr(data.msg.lastIndexOf('  ')).trim()
-            let robotAnswer = await getOpenAiReply(newMsg)
-            console.log(robotAnswer, 'robotAnswer')
+    switch (data.msg) {
+        case '聊天开启':
+            chatFlag = true
+            console.log("0000000000")
             return res.json(
                 {
                     "success": true,//true时，http-sdk才处理，false直接丢弃
                     "message": "successful!",
-                    "event": "SendGroupMsgAndAt",//告诉它干什么，SendImageMsg是发送图片事件
+                    "event": "SendTextMsg",//告诉它干什么，SendImageMsg是发送图片事件
                     "robot_wxid": data.robot_wxid,//用哪个机器人发
-                    "to_wxid": '',//发到哪里？群/好友
-                    "member_wxid": data.final_from_wxid,
-                    "member_name": data.final_from_name,
-                    "group_wxid": data.from_wxid,
-                    "msg": robotAnswer,//发送的内容
+                    "to_wxid": data.from_wxid,//发到哪里？群/好友
+                    "member_wxid": '',
+                    "member_name": '',
+                    "group_wxid": '',
+                    "msg": '聊天开启成功',//发送的内容
                 }
             )
-        }
+        case '聊天关闭':
+            chatFlag = true
+            console.log("11111111111")
+            return res.json(
+                {
+                    "success": true,//true时，http-sdk才处理，false直接丢弃
+                    "message": "successful!",
+                    "event": "SendTextMsg",//告诉它干什么，SendImageMsg是发送图片事件
+                    "robot_wxid": data.robot_wxid,//用哪个机器人发
+                    "to_wxid": data.from_wxid,//发到哪里？群/好友
+                    "member_wxid": '',
+                    "member_name": '',
+                    "group_wxid": '',
+                    "msg": '聊天关闭成功',//发送的内容
+                }
+            )
+        case '小姐姐':
+            return res.json(
+                {
+                    "success": true,//true时，http-sdk才处理，false直接丢弃
+                    "message": "successful!",
+                    "event": "SendImageMsg",//告诉它干什么，SendImageMsg是发送图片事件
+                    "robot_wxid": data.robot_wxid,//用哪个机器人发
+                    "to_wxid": data.from_wxid,//发到哪里？群/好友
+                    "member_wxid": '',
+                    "member_name": '',
+                    "group_wxid": '',
+                    "msg": {
+                        "url": 'https://api.vvhan.com/api/tao',
+                        "name": "111.jpg",
+                    },//发送的内容
+                }
+            )
+    }
+    /**微信聊天**/
+    if (!chatFlag) return //聊天关闭
+    let reg = /wxid=(.*)]/g
+    reg.test(data.msg)
+    let callSomeoneId = RegExp.$1
+    if (data?.msg?.indexOf('@at') > -1 && callSomeoneId == data.robot_wxid) { //要@机器人才聊天
+        const newMsg = data.msg.substr(data.msg.lastIndexOf('  ')).trim()
+        let robotAnswer = await getOpenAiReply(newMsg)
+        console.log(robotAnswer, 'robotAnswer')
+        return res.json(
+            {
+                "success": true,//true时，http-sdk才处理，false直接丢弃
+                "message": "successful!",
+                "event": "SendGroupMsgAndAt",//告诉它干什么，SendImageMsg是发送图片事件
+                "robot_wxid": data.robot_wxid,//用哪个机器人发
+                "to_wxid": '',//发到哪里？群/好友
+                "member_wxid": data.final_from_wxid,
+                "member_name": data.final_from_name,
+                "group_wxid": data.from_wxid,
+                "msg": robotAnswer,//发送的内容
+            }
+        )
+    }
 })
 
 
